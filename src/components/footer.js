e3c72b86870187f7c1632766a4fcda785f2f77ae
Luis Martins <luis.m.martins91@gmail.com>
2018-03-23 15:40:10 -0400

    Changed text in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom'
 export const Footer = () => (
     <div className="footer-component">
         <Link to="/design">
-            <h3 className="hidden-mobile hidden-tablet">Designer</h3>
-            <h3 className="hidden-desktop">Design</h3>
+            <h3>Designer</h3>
         </Link>
         <h3 className="separator">•</h3>
         <Link to="/photography">
-            <h3 className="hidden-mobile hidden-tablet">Photographer</h3>
-            <h3 className="hidden-desktop">Photography</h3>
+            <h3>Photographer</h3>
         </Link>
     </div>
 )
